2fe1cb9d0340646b514e0a2cd67203ce5b7545e5
antiloquax <tranter.m@sky.com>
2023-03-17 08:51:34 +0000

    15

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ async function send(entry, client) {
   let result;
   try {
     result = await sendComment(entry, client);
-    console.log(result);
   } catch (error) {
     console.log(error);
     return false;
