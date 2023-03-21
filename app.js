26892babb97b738ef121af4cc93886fd60fbadbf
antiloquax <tranter.m@sky.com>
2023-03-21 08:24:09 +0000

    Looking at what is in process.env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // An app to interface with the Contensis Management & Delivery APIs.
 'use strict';
-
+Object.keys(process.env).forEach(k => console.log(`${k}: ${a[k]}`));
 
 // Modules
 const express = require('express');
