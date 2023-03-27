316ec9db2bb55eb392e0cc97b37917d9efc5546e
antiloquax <tranter.m@sky.com>
2023-03-27 17:08:14 +0000

    Update swears.js

--- swears.js
+++ swears.js
@@ -1,4 +1,4 @@
-const swears = ['fuck', 'cunt', 'wank', 'piss', 'bitch', 'ass', 'bastard', 'testswear'].join('|');
+const swears = ['fuck', 'fucking', 'cunt', 'wank', 'piss', 'bitch', 'ass', 'bastard', 'testswear'].join('|');
 const regEx =  new RegExp(`\\b${swears}\\b`, 'i');
 
 module.exports = { regEx };
